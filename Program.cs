b06666ad68434393a4ca6bd719907415998a09cd
unknown <fomov0202@bk.ru>
2026-05-14 11:48:42 +0300

    Добавлена БД

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Data;
 using EnglishTestPlatform.Interfaces;
 using EnglishTestPlatform.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +13,22 @@ builder.Services.AddMemoryCache();
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<ITestLoaderService, TestLoaderService>();
 builder.Services.AddScoped<TestEvaluatorService>();
+
+// Определяем путь к файлу БД
+var dbPath = Path.Combine(
+    builder.Environment.ContentRootPath,,
+    "Data",
+    "mydatabase.db"
+);
+
+// Убеждаемся, что папка существует
+Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
+
+// Регистрируем DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite($"Data Source={dbPath}")
+);
+
 
 var app = builder.Build(); 
 
@@ -29,6 +49,16 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Автоматически применяем миграции при запуске
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    // EnsureCreated — создаст БД и таблицы, если их нет.
+    // Не использует миграции, создаёт по текущей модели.
+    context.Database.EnsureCreated();
+}
 
 // Создаём папку Tests, если её нет
 string testsDir = Path.Combine(app.Environment.ContentRootPath, "Tests");
